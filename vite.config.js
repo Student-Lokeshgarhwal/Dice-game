c1c2922b795472424db2ffca9eafcf3dc2d78c86
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-09-09 21:57:34 +0530

    fix: set vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Dice-game/",
   plugins: [react()],
+  base: "./", // Add this line!
 })
